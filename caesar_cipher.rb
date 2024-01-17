21871e65515dd91b72f8728b0a4bc34234a11280
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2024-01-17 14:47:18 +0800

    build: add puts to caesar_cipher

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -7,4 +7,4 @@ cipher = gets.chomp.to_s
 print "Input a Number (Key): "
 key = gets.chomp.to_i
 
-caesar_cipher(cipher = "What a string!", key = 5)
+puts caesar_cipher(cipher = "What a string!", key = 5)
