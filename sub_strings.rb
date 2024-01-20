6003264e4b1bad852a9774552ed528f8aab761f4
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2024-01-20 17:34:41 +0800

    build: variable result containing hash

--- sub_strings.rb
+++ sub_strings.rb
@@ -1,5 +1,6 @@
 def substrings(string, dictionary)
     string.downcase!
+    result = Hash.new(0)
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
