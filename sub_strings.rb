0c2e7b49408db43c9eb410f616de99c8abf211aa
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2024-01-20 21:14:45 +0800

    fix: return result

--- sub_strings.rb
+++ sub_strings.rb
@@ -6,6 +6,7 @@ def substrings(string, dictionary)
             result[sub_string] = string.scan(sub_string).length
         end
     end
+    result
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
