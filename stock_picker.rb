fe5548475ba5a1700e23a730083e8014346e0d11
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2024-01-27 19:17:23 +0800

    build: iterate prices one by one

--- stock_picker.rb
+++ stock_picker.rb
@@ -5,8 +5,14 @@ def stock_picker(array)
 
     max_profit = 0
     best_day = [] 
+
+    array.each_with_index do |best_to_buy, buy_day|
+        array.each_with_index do |best_to_sell, sell_day|
+        
+        end
+    end
 end
 
-stock_picker([17,3,6,9,15,8,6,1,10])
+p stock_picker([17,3,6,9,15,8,6,1,10])
 # expected output
 # [1,4]  # for a profit of $15 - $3 == $12
