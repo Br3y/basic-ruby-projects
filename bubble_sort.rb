cd7adf2ede0946020dbd771cc97c089129a6cbbf
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2024-01-29 14:10:20 +0800

    feat: swap value if array[i] is greater than array[j]

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,7 +2,11 @@ def bubble_sort(array)
     num = array.length
     (0...num).each do |i|
         (i+1...num).each do |j|
-             
+             if array[i] > array[j]
+                temp = array[i]
+                array[i] = array[j]
+                array[j] = temp
+             end
         end
     end
 end
